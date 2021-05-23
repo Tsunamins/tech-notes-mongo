d5e8476361f4d40e67319d056f066039e0e33a3a
Alison Reilly <reillyamr@gmail.com>
2021-05-23 19:03:01 -0400

    add general retrieve all code with query

--- dao/notesDAO.js
+++ dao/notesDAO.js
@@ -8,7 +8,7 @@ export default class NotesDAO {
         return
       }
       try { 
-        users = await conn.db(process.env.TECHNOTES_NS).collection("notes")
+        notes = await conn.db(process.env.TECHNOTES_NS).collection("notes")
       } catch (e) {
         console.error(
           `Unable to establish a collection handle in notesDAO: ${e}`,
@@ -16,6 +16,46 @@ export default class NotesDAO {
       }
     }
 
-
+    static async getNotes({
+        filters = null, //can add this to filter, pass a filter in to trigger more of the code below for filters
+        page = 0, //page specified if appl, default
+        notesPerPage = 20, //get 20 at a time, default
+      } = {}) {
+        let query
+        //these below will just depend on which filter was passed in
+        if (filters) { //if type of search is requested, i.e. search by name, more on queirying in mongo check resources in vid
+          if ("title" in filters) {
+            query = { $text: { $search: filters["title"] } }
+          } else if ("description" in filters) {
+            query = { "description": { $eq: filters["description"] } }
+          } else if ("note_type" in filters) {
+            query = { "note_type": { $eq: filters["note_type"] } }
+          }
+        }
+    
+        let cursor
+        
+        try {
+          cursor = await notes
+            .find(query)
+        } catch (e) {
+          console.error(`Unable to issue find command, ${e}`)
+          return { notesList: [], totalNumNotes: 0 }
+        }
+    
+        const displayCursor = cursor.limit(notesPerPage).skip(notesPerPage * page)
+    
+        try {
+          const notesList = await displayCursor.toArray()
+          const totalNumNotes = await notes.countDocuments(query)
+    
+          return { notesList, totalNumNotes }
+        } catch (e) {
+          console.error(
+            `Unable to convert cursor to array or problem counting documents, ${e}`,
+          )
+          return { notesList: [], totalNumNotes: 0 }
+        }
+      }
 
 }
--- dao/usersDAO.js
+++ dao/usersDAO.js
@@ -16,6 +16,51 @@ export default class UsersDAO {
       }
     }
 
+    // add a general get users, update later
+    static async getUsers({
+        filters = null, 
+        page = 0,
+        usersPerPage = 20, 
+      } = {}) {
 
+        let query
+
+        if (filters) { 
+          if ("name" in filters) {
+            query = { $text: { $search: filters["username"] } }
+          } else if ("email" in filters) {
+            query = { "email": { $eq: filters["email"] } }
+          } else if ("id" in filters) {
+            query = { "id": { $eq: filters["id"] } }
+          }
+        }
+
+        let cursor
+
+        try {
+            cursor = await users
+              .find(query)
+          } catch (e) {
+            console.error(`Unable to issue find command, ${e}`)
+            return { usersList: [], totalNumUsers: 0 }
+          }
+
+          const displayCursor = cursor.limit(usersPerPage).skip(usersPerPage * page)
+
+          try {
+            const usersList = await displayCursor.toArray()
+            const totalNumUsers = await users.countDocuments(query)
+      
+            return { usersList, totalNumUsers }
+          } catch (e) {
+            console.error(
+              `Unable to convert cursor to array or problem counting documents, ${e}`,
+            )
+            return { usersList: [], totalNumUsers: 0 }
+          }
+        }
+
+        
 
 }
+
