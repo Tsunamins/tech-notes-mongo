81560a7c6ebbaf189f77d84f9d4fb64bb130d0ff
Alison Reilly <reillyamr@gmail.com>
2021-05-23 18:46:23 -0400

    add basic code for connection to data

--- dao/notesDAO.js
+++ dao/notesDAO.js
@@ -1,2 +1,21 @@
 import mongodb from "mongodb"
 const ObjectId = mongodb.ObjectID
+let notes
+
+export default class NotesDAO {
+    static async injectDB(conn) {
+      if (notes) {
+        return
+      }
+      try { 
+        users = await conn.db(process.env.TECHNOTES_NS).collection("notes")
+      } catch (e) {
+        console.error(
+          `Unable to establish a collection handle in notesDAO: ${e}`,
+        )
+      }
+    }
+
+
+
+}
