dfbf50594c8f66a3354cbab39b98b5f9d888a0ea
Alison Reilly <reillyamr@gmail.com>
2021-06-12 12:49:24 -0400

    updated missing route for users

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from "express"
 import cors from "cors"
 import notes from "./api/notes.route.js"
+import users from "./api/users.route.js"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 //specify routes
-app.use("/api/v1/notes", notes) 
+app.use("/api/v1/notes", notes)
+app.use("/api/v1/users", users)
 app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
 
 export default app
